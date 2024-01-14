609dab83a9dd6832140664f56a46f2db4c08cc97
이승규 <unhappydogchew@gmail.com>
2024-01-14 19:28:13 +0900

    feat: 메인 페이지 레이아웃 정리

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [mycount, setMycount] = useState(0);
 
   return (
     <>
-      <h1>Hello, react + vite!</h1>
+      <header>
+        <h1>🌇 devday.log</h1>
+        <hr/>
+      </header>
+      <main>
+        <p>main blog content here</p>
+      </main>
     </>
   )
 }
